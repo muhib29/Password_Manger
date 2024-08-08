87b6295d0c9e79e349af62290bef725b39a3fd6e
muhib29 <146656594+muhib29@users.noreply.github.com>
2024-08-08 10:23:46 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+import './App.css'
+import Navbar from './Components/Navbar'
+import Manager from './Components/Manager'
+import Footer from './Components/Footer'
+function App() {
+
+  return (
+    <>
+     <Navbar />
+     <div className="bg-green-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+       <Manager />
+      </div>
+     <Footer />
+    </>
+  )
+}
+
+export default App
+
+
+// Express.js
+// backend
+// *********
+// https://www.npmjs.com/package/dotenv
+// backend
+// *********
+// https://www.npmjs.com/package/mongodb
+// backend
+// *********
+// https://expressjs.com/en/guide/overriding-express-api.html
+// backend
+// *********
+// Tailwind.css
+// frontend
+// ******
+// POST MAN
+// backend
+// *********
+// https://lordicon.com/
+// icons
+// ****
+// https://www.npmjs.com/package/react-toastify
+// Message
+// ****
+// https://www.npmjs.com/package/uuid
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-slate-800 text-center text-white sticky bottom-0 w-full'>
+        <div className="logo font-bold text-white text-2xl ">
+        <span className="text-green-500 "> &lt;</span>
+        Pass
+        <span className="text-green-500">OP/&gt;</span>
+              </div>
+              <span className='flex text-center items-center justify-center'>Created With <img className='w-10 p-2 bg-slate-800 text-black' src="icons/heart.png" alt="" />By Syed Muhib</span>
+    </div>
+  )
+}
+
+export default Footer
